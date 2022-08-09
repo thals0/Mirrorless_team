cac011c998a20a136efc16178538d17fd3fab5f3
MO <48742497+msh9900@users.noreply.github.com>
2022-08-09 18:33:35 +0900

    0809일 수정

---
+++ seunghwan/js/video.js
@@ -0,0 +1,12 @@
+let camera1 =document.querySelector(".img1");
+let camera2 =document.querySelector(".img2");
+
+
+
+function cameraOpcity(){
+    camera1.style.opacity = 1;
+    camera2.style.opacity = 1;
+
+}
+
+setTimeout(cameraOpcity, 9000);
