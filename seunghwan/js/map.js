2a28fd4d62bfe0b19a26fafec8df4a18addba674
MO <48742497+msh9900@users.noreply.github.com>
2022-08-11 19:08:57 +0900

    8월11일 수정 모승환

--- seunghwan/js/map.js
+++ seunghwan/js/map.js
@@ -126,29 +126,177 @@ var positions = [
         '  </a>' +
         '</div>'
 
-    }
+    },
+    {
+        title: '용산카메라',
+        latlng: new kakao.maps.LatLng(37.52956489999985, 126.96430409999968),
+        link: 'https://blog.naver.com/yangkun100',
+        content: '<div class="customoverlay">' +
+        '  <a href=https://blog.naver.com/yangkun100>' +
+        '    <span class="title">용산카메라</span>' +
+        '  </a>' +
+        '</div>'
+
+    },
+    {
+        title: '즐거운카메라',
+        latlng: new kakao.maps.LatLng(37.5326643999998, 126.95806069999986),
+        link: 'https://www.fu-gall.com/',
+        content: '<div class="customoverlay">' +
+        '  <a href=https://www.fu-gall.com/>' +
+        '    <span class="title">즐거운카메라</span>' +
+        '  </a>' +
+        '</div>'
+
+    },
+    {
+        title: 'UA카메라',
+        latlng: new kakao.maps.LatLng(37.5354644999999, 127.09517899999949),
+        link: 'https://www.uacamera.co.kr/',
+        content: '<div class="customoverlay">' +
+        '  <a href=https://www.uacamera.co.kr/>' +
+        '    <span class="title">UA카메라</span>' +
+        '  </a>' +
+        '</div>'
+
+    },
+    {
+        title: '반도카메라',
+        latlng: new kakao.maps.LatLng(37.502388099999806, 127.05504770000013),
+        link: 'https://booking.naver.com/booking/10/bizes/655045?area=bmp',
+        content: '<div class="customoverlay">' +
+        '  <a href=https://booking.naver.com/booking/10/bizes/655045?area=bmp>' +
+        '    <span class="title">반도카메라</span>' +
+        '  </a>' +
+        '</div>'
+
+    },
+    {
+        title: '럭키카메라',
+        latlng: new kakao.maps.LatLng(37.53572629999982, 127.09466189999938),
+        link: '#',
+        content: '<div class="customoverlay">' +
+        '  <a href=#>' +
+        '    <span class="title">럭키카메라</span>' +
+        '  </a>' +
+        '</div>'
+
+    },
+    {
+        title: '라이카카메라스토어',
+        latlng: new kakao.maps.LatLng(37.392494799999625, 127.11144640000012),
+        link: 'https://www.leica-store.co.kr/main/main.asp',
+        content: '<div class="customoverlay">' +
+        '  <a href=https://www.leica-store.co.kr/main/main.asp>' +
+        '    <span class="title">라이카카메라스토어</span>' +
+        '  </a>' +
+        '</div>'
+
+    },
+    {
+        title: '캐논 현대백화점',
+        latlng: new kakao.maps.LatLng(37.50865419999967, 127.05137089999968),
+        link: 'https://kr.canon/intro',
+        content: '<div class="customoverlay">' +
+        '  <a href=https://kr.canon/intro>' +
+        '    <span class="title">캐논 현대백화점</span>' +
+        '  </a>' +
+        '</div>'
+
+    },
+    {
+        title: '어필룸',
+        latlng: new kakao.maps.LatLng(37.58208320000015, 126.9705099),
+        link: 'https://www.instagram.com/uh_feel/',
+        content: '<div class="customoverlay">' +
+        '  <a href=https://www.instagram.com/uh_feel/>' +
+        '    <span class="title">어필룸</span>' +
+        '  </a>' +
+        '</div>'
+
+    },
+    {
+        title: '필름로그',
+        latlng: new kakao.maps.LatLng(37.50040079999985, 127.02493049999941),
+        link: 'https://www.instagram.com/filmlog_official/',
+        content: '<div class="customoverlay">' +
+        '  <a href=https://www.instagram.com/filmlog_official/>' +
+        '    <span class="title">필름로그</span>' +
+        '  </a>' +
+        '</div>'
+
+    },
+    {
+        title: '캐논플렉스',
+        latlng: new kakao.maps.LatLng(37.50780149999977, 127.0348027999998),
+        link: 'https://blog.naver.com/canonkoreacamera',
+        content: '<div class="customoverlay">' +
+        '  <a href=https://blog.naver.com/canonkoreacamera>' +
+        '    <span class="title">캐논플렉스</span>' +
+        '  </a>' +
+        '</div>'
+
+    },
+
 ];
 
 // 마커 이미지의 이미지 주소입니다
-var imageSrc = "./img/마커이미지.png"; 
+
     
 for (var i = 0; i < positions.length; i ++) {
     
     // 마커 이미지의 이미지 크기 입니다
-    
+
+    var imageSrc = "./img/마커이미지.png";
+    var imageSrc2 = "./img/마커이미지2.png";
+    var imageSrc3 = "./img/마커이미지3.png";
+    var imageSrc4 = "./img/마커이미지4.png";  
+
     var imageSize = new kakao.maps.Size(40, 40); // 마커이미지의 크기입니다
     var imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다. 
     
     // 마커 이미지를 생성합니다    
-    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize ,imageOption); 
-    
-    // 마커를 생성합니다
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize ,imageOption);
+    var markerImage2 = new kakao.maps.MarkerImage(imageSrc2, imageSize ,imageOption); 
+    var markerImage3 = new kakao.maps.MarkerImage(imageSrc3, imageSize ,imageOption); 
+    var markerImage4 = new kakao.maps.MarkerImage(imageSrc4, imageSize ,imageOption); 
+
+    if(i < 4){
+        var marker = new kakao.maps.Marker({
+            map: map, // 마커를 표시할 지도
+            position: positions[i].latlng, // 마커를 표시할 위치
+            title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            image : markerImage // 마커 이미지
+        });
+    }
+    else if(i < 8){
+            // 마커를 생성합니다
     var marker = new kakao.maps.Marker({
         map: map, // 마커를 표시할 지도
         position: positions[i].latlng, // 마커를 표시할 위치
         title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-        image : markerImage // 마커 이미지
+        image : markerImage2 // 마커 이미지
     });
+    }
+    else if(i <12){
+            // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        map: map, // 마커를 표시할 지도
+        position: positions[i].latlng, // 마커를 표시할 위치
+        title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+        image : markerImage3 // 마커 이미지
+    });
+    }
+    else{
+            // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        map: map, // 마커를 표시할 지도
+        position: positions[i].latlng, // 마커를 표시할 위치
+        title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+        image : markerImage4 // 마커 이미지
+    });
+    }
+
 //     var content = '<div class="customoverlay">' +
 //     '  <a href='+positions[i].link+'>' +
 //     '    <span class="title">'+positions[i].title+ '</span>' +
@@ -193,14 +341,87 @@ function makeOverListener(map, marker, infowindow) {
 //         infowindow.close();
 //     };
 // }
+let input1 = document.querySelector(".input1");
+let inputbox1 = document.querySelector(".input_box1")
+let inputText1 =document.querySelectorAll(".input_text1");
+let input1I = document.querySelector(".input1 i");
+let input2 = document.querySelector(".input2");
+let inputbox2 = document.querySelector(".input_box2")
+let inputText2 =document.querySelectorAll(".input_text2");
+let input2I = document.querySelector(".input2 i");
+let input3 = document.querySelector(".input3");
+let inputbox3 = document.querySelector(".input_box3")
+let inputText3 =document.querySelectorAll(".input_text3");
+let input3I = document.querySelector(".input3 i");
+let input4 = document.querySelector(".input4");
+let inputbox4 = document.querySelector(".input_box4")
+let inputText4 =document.querySelectorAll(".input_text4");
+let input4I = document.querySelector(".input4 i");
 
-let maplistContent = document.querySelectorAll(".maplist div");
+    input1.addEventListener("click",function(){
+        if(inputbox1.style.display === "block"){
+            inputbox1.style.display = "none";
+            input1I.style.transform = "rotateX(0deg)";
 
-for(let i =0; i<maplistContent.length;i++){
-    maplistContent[i].addEventListener("mouseover",function(e){
-        e.currentTarget.classList.add("maphover");
+        }
+        else{
+            inputbox1.style.display = "block";
+            input1I.style.transform = "rotateX(180deg)";
+        }
+        for(let j =0; j<inputText1.length;j++){
+            inputText1[j].addEventListener("click",function(e){
+                let text =e.currentTarget.textContent
+            })
+        }
     })
-    maplistContent[i].addEventListener("mouseout",function(e){
-        e.currentTarget.classList.remove("maphover");
+    input2.addEventListener("click",function(){
+        if(inputbox2.style.display === "block"){
+            inputbox2.style.display = "none"
+            input2I.style.transform = "rotateX(0deg)";
+
+        }
+        else{
+            inputbox2.style.display = "block"
+            input2I.style.transform = "rotateX(180deg)";
+        }
+        for(let j =0; j<inputText2.length;j++){
+            inputText2[j].addEventListener("click",function(e){
+                let text =e.currentTarget.textContent
+            })
+        }
     })
-}
+    input3.addEventListener("click",function(){
+        if(inputbox3.style.display === "block"){
+            inputbox3.style.display = "none"
+            input3I.style.transform = "rotateX(0deg)";
+
+        }
+        else{
+            inputbox3.style.display = "block"
+            input3I.style.transform = "rotateX(180deg)";
+        }
+        for(let j =0; j<inputText3.length;j++){
+            inputText3[j].addEventListener("click",function(e){
+                let text =e.currentTarget.textContent
+            })
+        }
+    })
+    input4.addEventListener("click",function(){
+        if(inputbox4.style.display === "block"){
+            inputbox4.style.display = "none"
+            input4I.style.transform = "rotateX(0deg)";
+
+        }
+        else{
+            inputbox4.style.display = "block"
+            input4I.style.transform = "rotateX(180deg)";
+        }
+        for(let j =0; j<inputText4.length;j++){
+            inputText4[j].addEventListener("click",function(e){
+                let text =e.currentTarget.textContent
+            })
+        }
+    })
+
+
+
--- seunghwan/js/video.js
+++ seunghwan/js/video.js
@@ -1,15 +1,18 @@
 let camera1 =document.querySelector(".img1");
 let camera2 =document.querySelector(".img2");
 let video = document.querySelector(".video")
+let title1 = document.querySelector(".title1");
+let title2 = document.querySelector(".title2");
+let title3 = document.querySelector(".title3");
 
 
-function cameraOpcity(){
-    camera1.style.opacity = 1;
-    camera2.style.opacity = 1;
+// function cameraOpcity(){
+//     camera1.style.opacity = 1;
+//     camera2.style.opacity = 1;
 
-}
+// }
 
-setTimeout(cameraOpcity, 9000);
+// setTimeout(cameraOpcity, 9000);
 
 camera1.addEventListener('mouseover', function(){
     camera1.style.transform = "scale(1.5)";
@@ -25,13 +28,6 @@ camera1.addEventListener('click', function(){
     camera2.style.opacity ="0";
     setTimeout("location.href='map.html'", 3500);
   });
-camera2.addEventListener('click', function(){
-    video.setAttribute("src","./video/videoreverse.mp4");
-    camera1.style.opacity ="0";
-    camera2.style.opacity ="0";
-    setTimeout("location.href='map.html'", 3500);
-  });
-  
 camera2.addEventListener('mouseover', function(){
     camera2.style.transform = "scale(1.5)";
     camera2.style.transition = "0.5s";
@@ -39,4 +35,76 @@ camera2.addEventListener('mouseover', function(){
 camera2.addEventListener('mouseout', function(){
     camera2.style.transform = "scale(1)";
     camera2.style.transition = "0.5s";
-  });  
+  });
+
+  // let scrollYpos;
+  // window.addEventListener("scroll", function(){
+  //     scrollYpos = window.scrollY;
+  //     if(scrollYpos >1200){
+  //       title1.style.transform = "translate(0px,0px)"
+  //       title2.style.transform = "translate(0px,0px)"
+  //       title3.style.transform = "translate(0px,0px)"
+  //     }
+  //     console.log(scrollYpos);
+  // })
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.from(".title1", {
+     scrollTrigger: {
+      trigger: ".section",
+      toggleActions: "restart",
+      start: "30% 0%",
+      end: "50% 50%",
+     },
+     x: -2000,
+     scaleX: 1,
+     duration: 2,
+    });
+  gsap.from(".title2", {
+    scrollTrigger: {
+     trigger: ".section",
+     toggleActions: "restart",
+     start: "30% 0%",
+     end: "50% 50%",
+    },
+    x: 2000,
+    scaleX: 1,
+    duration: 2,
+   });
+   gsap.from(".title3", {
+    scrollTrigger: {
+     trigger: ".section",
+     toggleActions: "restart",
+     start: "30% 0%",
+     end: "50% 50%",
+    },
+    x: 2000,
+    scaleX: 1,
+    duration: 2,
+   });
+   gsap.to(".img1", {
+    scrollTrigger: {
+     trigger: ".section",
+     toggleActions: "restart",
+     start: "30% 0%",
+     end: "50% 50%",
+    },
+    x: 0,
+    scaleX: 1,
+    duration: 2,
+    opacity: 1,
+   })
+   gsap.to(".img2", {
+    scrollTrigger: {
+     trigger: ".section",
+     toggleActions: "restart",
+     start: "30% 0%",
+     end: "50% 50%",
+    },
+    x: 0,
+    scaleX: 1,
+    duration: 2,
+    opacity: 1,
+
+   })  
+     
+     
